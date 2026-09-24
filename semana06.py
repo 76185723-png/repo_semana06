3b6ca11fe6895b709869e9b352c016a6bde3cb81
Jhoel <76185723@institutourusayhua.edu.pe>
2026-09-24 11:21:36 -0500

    comflicto solucionado

--- semana06.py
+++ semana06.py
@@ -1,6 +1,6 @@
-# COMENTARIO DESDE LA RAMA rama-semana7
+# COMENTARIO DESDE LA RAMA rama-semana7 cambio en main
 print("HOLA DESDE LA PC")
 
-print("HOLA DESDE LA RAMA mensaje-mejora")
+print("HOLA DESDE LA RAMA mensaje-mejora")  
 
 print("HOLA DESDE LA RAMA jhoel")
